dfa34a02c9edf9e39cdfd1191efa2c43effb9742
AnotherNoName <65259492+AnotherNoName@users.noreply.github.com>
2022-12-27 14:05:40 -0400

    Folder structure + Assets

--- org/json/simple/JSONAware.java
+++
@@ -1,9 +0,0 @@
-/*
- * Decompiled with CFR 0.150.
- */
-package org.json.simple;
-
-public interface JSONAware {
-    public String toJSONString();
-}
-
--- org/spongepowered/tools/obfuscation/interfaces/IOptionProvider.java
+++
@@ -1,9 +0,0 @@
-/*
- * Decompiled with CFR 0.150.
- */
-package org.spongepowered.tools.obfuscation.interfaces;
-
-public interface IOptionProvider {
-    public String getOption(String var1);
-}
-
